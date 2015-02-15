31ae2eeb78d85afd63025b7bc6aee1e785b0b823
James Newell <james@digitaledgeit.com.au>
2015-02-15 22:34:29 +1100

    Revert "made token methods accessible without running the plugin"
    This reverts commit f7392ecf1037edde2aa64ed5aaf4f577f70cbcce.

--- index.js
+++ index.js
@@ -84,83 +84,83 @@ module.exports = function(options) {
 
 		});
 
-	};
-
-	/**
-	 * Get the request token
-	 * @param   {function(Error, Object)} callback
-	 * @returns {plugin}
-	 */
-	plugin.getRequestToken = function(callback) {
-		var req = client.post('/oauth/request_token');
-
-		if (options.callback_url) {
-			req.getUrl().getQuery().oauth_callback = options.callback_url;
-		}
-
-		client.use(body.urlencoded({once: true}));
-
-		client.send(req, function(error, response) {
-			if (error) return callback(error);
-
-			if (response.getStatus() !== 200) {
-				return callback(undefined, new Error('Invalid response status'));
-			}
-
-			var body = response.getBody();
-
-			callback(undefined, {
-				token:        body.oauth_token,
-				token_secret: body.oauth_token_secret
-			});
-
-		});
-
-		return this;
-	};
-
-	/**
-	 * Get the authorisation URL
-	 * @param   {function(Error, Object)} callback
-	 * @returns {plugin}
-	 */
-	plugin.getAuthorisationUrl = function(callback) {
-		this.getRequestToken(function(error, token) {
-			if (error) return callback(error);
-
-			var query = {oauth_token: token.token};
+		/**
+		 * Get the request token
+		 * @param   {function(Error, Object)} callback
+		 * @returns {plugin}
+		 */
+		plugin.getRequestToken = function(callback) {
+			var req = client.post('/oauth/request_token');
 
 			if (options.callback_url) {
-				query.callback_url = options.callback_url;
+				req.getUrl().getQuery().oauth_callback = options.callback_url;
 			}
 
-			var url = URL.format({
-				pathname: '/oauth/authorize',
-				query:    query
+			client.use(body.urlencoded({once: true}));
+
+			client.send(req, function(error, response) {
+				if (error) return callback(error);
+
+				if (response.getStatus() !== 200) {
+					return callback(undefined, new Error('Invalid response status'));
+				}
+
+				var body = response.getBody();
+
+				callback(undefined, {
+					token:        body.oauth_token,
+					token_secret: body.oauth_token_secret
+				});
+
 			});
 
-			callback(undefined, url);
-		});
-		return this;
-	};
+			return this;
+		};
 
-	/**
-	 * Get the access token
-	 * @param   {Object}                  token     The request token
-	 * @param   {function(Error, Object)} callback
-	 * @returns {plugin}
-	 */
-	plugin.getAccessToken = function(token, callback) {
-		'/oauth/access_token?oauth_token=&oauth_verifier=';
-		var req = client.post('/oauth/access_token');
+		/**
+		 * Get the authorisation URL
+		 * @param   {function(Error, Object)} callback
+		 * @returns {plugin}
+		 */
+		plugin.getAuthorisationUrl = function(callback) {
+			this.getRequestToken(function(error, token) {
+				if (error) return callback(error);
 
-		req.getUrl().getQuery().oauth_token = token.token;
+				var query = {oauth_token: token.token};
 
-		if (options.callback_url) {
-			req.getUrl().getQuery().oauth_callback = +options.callback_url;
-		}
+				if (options.callback_url) {
+					query.callback_url = options.callback_url;
+				}
 
-		client.send(req, callback);
+				var url = URL.format({
+					pathname: '/oauth/authorize',
+					query:    query
+				});
+
+				callback(undefined, url);
+			});
+			return this;
+		};
+
+		/**
+		 * Get the access token
+		 * @param   {Object}                  token     The request token
+		 * @param   {function(Error, Object)} callback
+		 * @returns {plugin}
+		 */
+		plugin.getAccessToken = function(token, callback) {
+			'/oauth/access_token?oauth_token=&oauth_verifier=';
+			var req = client.post('/oauth/access_token');
+
+			req.getUrl().getQuery().oauth_token = token.token;
+
+			if (options.callback_url) {
+				req.getUrl().getQuery().oauth_callback = +options.callback_url;
+			}
+
+			client.send(req, callback);
+
+		};
 
 	};
 
